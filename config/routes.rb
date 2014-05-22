01078050f776c3fe524f51853f21971be3a46599
jcstewart09 <vagrant@kellogg-dev-box>
2014-05-22 01:16:24 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Final::Application.routes.draw do
+
+  get "/" => "fitness#home"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+
+
+Activity.delete
+Activity.create("activity" => "boxing")
+
+Type.delete
+Type.create("type" => "Strengh Heavy")
+
+
+Location.delete
+Location.create("name" => "Evanston Athletic Club (EAC)", "Address" => "1723 Benson Ave, Evanston, IL 60201, (847) 866-6190", "loc_photo" => "https://chicagoathleticclubs.com/wp-content/gallery/evanston/eac90.jpg",  )
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
